ca8af2d475bb578c52e0034435f9912ab7d01267
László Pálfalvi <palflaszlo20@outlook.hu>
2020-06-10 19:06:08 +0200

    Add project files.

---
+++ Pálfalvi László - KPMG/Menubar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+//------< using >------
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+//------</ using >------
+
+
+namespace Pálfalvi_László___KPMG
+{
+    public partial class Menubar
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void MNB_adatletoltes_Click(object sender, RibbonControlEventArgs e)
+        {
+            //< open Excel Worksheet >
+            Excel.Worksheet activeWorksheet = Globals.ThisAddIn.Application.ActiveSheet;
+            //< open Excel Worksheet >
+
+            //< get Cell.Value > 
+            Excel.Range actCell = Globals.ThisAddIn.Application.ActiveCell;
+            if(actCell.Value2 != null)
+            {
+                string sValue = actCell.Value2.ToString();
+                string sText  = actCell.Text;
+                System.Windows.Forms.MessageBox.Show(sText);
+            }
+
+            //</ get Cell.Value > 
+
+        }
+    }
+}
